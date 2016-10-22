44bcd9803b8e436abcca78203a06fcff2547763e
snakazawa <nakazawa.tms0314@gmail.com>
2016-10-22 18:49:22 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# tacowasa-mobile
+Web based user interface for mobile of tacowasa
+
+[![Build Status](https://travis-ci.org/Kait-tt/tacowasa-mobile.svg?branch=master)](https://travis-ci.org/Kait-tt/tacowasa-mobile)
+
+# requirements
+- node v6
+- npm
+
+# install
+```
+git clone https://github.com/Kait-tt/tacowasa
+cd tacowasa-mobile
+npm install
+npm run build
+```
+
+# scripts
+see `package.json`
+
+- `npm run build` : build public files with webpack
+- `npm run watch` : watch to build
+- `npm run lint:all` : run eslint for all js files
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+// development
+const webpackConfig = {
+    entry: {
+        index: path.join(__dirname, '/src/js/entries/index.js')
+    },
+    output: {
+        publicPath: '/',
+        path: path.join(__dirname, '/dist'),
+        filename: '[name].js'
+    },
+    module: {
+        loaders: [
+            // html loaderの設定をここに定義するとscssの読込がバグるのでやらないこと
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel'
+            },
+            {
+                test: /\.scss$/,
+                loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader')
+            }
+        ]
+    },
+    htmlLoader: {
+        attrs: ['minimize']
+    },
+    sassLoader: {
+        includePaths: [path.join(__dirname, '/src/scss')]
+    },
+    devtool: '#source-map',
+    resolve: {
+        extensions: ['', '.js', '.json']
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin('common.js'),
+        new ExtractTextPlugin('[name].css'),
+        function () {
+            this.plugin('watch-run', (watching, callback) => {
+                console.log('Begin compile at ' + new Date());
+                callback();
+            });
+        }
+    ]
+};
+
+module.exports = webpackConfig;
